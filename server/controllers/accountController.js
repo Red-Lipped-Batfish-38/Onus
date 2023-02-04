59738637e7fc4cb2eedaeb4ff4f3c0507de118a2
Mark Krasner <markkrasner@Marks-MacBook-Pro.local>
2023-02-04 14:36:46 -0500

    commit for finalizing authentication

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -29,19 +29,65 @@ accountController.createAccount = (req, res, next) => {
 }
 
 
-accountController.verifyUser = (res, req, next) => {
-    console.log('in verify user')
-    const (firstName, lastName, email, password) = req.body;
-    if(!firstName || !lastName || !email || !password){
-        return next('Missing credentials');
+accountController.verifyUser = (req, res, next) => {
+    console.log('in verify user');
+    //user logs in with email and password
+    const {email, password} = req.body;
+
+
+      const controller = (passPhrase) => {
+      
+        console.log(passPhrase, 'this is the password');
+        Account.find({email}, 'password').exec()
+          .then((data) =>{
+            console.log(data);
+            //compare plaintext pw and encrypted
+            bcrypt.compare(passPhrase, data[0].password, function(err, res) {
+              console.log(res, 'this is the res');
+              if(err){
+                return next({
+                    log: 'Error occurred in the accountController.verifyUser middleware',
+                    status: 400,
+                    err: { err: 'Either the password or username is incorrect'}
+                  });
+              } 
+            });
+            
+            next();
+          })
+          .catch(err => {
+            next({
+                log: 'Error occurred in the accountController.verifyUser middleware',
+                status: 400,
+                err: { err: 'Unknown error'}
+            });
+          });
       }
-    //find email and associated pw
-     //then save to encryoed pw
-    
-    //get the pasword
-    bcrypt.compare(password, ) //compare to encrypted
-    //bcrypt.compare the plaintext pw and encrypted
-      //errm result, send result next
+
+    controller(password);
 }
+
+//for auto-checking user exists based on browser cookie upon useEffect react hook
+accountController.checkUser = (req, res, next) => {
+    const ObjectId = require('mongodb').ObjectId; 
+    const id = req.cookies.ssid;     
+    const o_id = new ObjectId(id);
+    
+    Account.find({_id:o_id}).exec()
+      .then((data) => {
+        console.log('we got into the check user method');
+        //console.log(data);
+        const {firstName, lastName, email} = data[0];
+        res.locals.userSession = {firstName, lastName, email};
+        next();
+      })
+      .catch(err => {
+        next({
+            log: 'Error occurred in the accountController.checkUser middleware',
+            status: 400,
+            err: { err: 'Unknown cookie error'}
+        });
+      });
+  };
 
 module.exports = accountController;
--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -7,6 +7,10 @@ const sessionController = {};
 */
 sessionController.startSession = (req, res, next) => {
     //access locals property from previous middleware chain
+    //console.log(res.locals.ssid, 'This is from startsession res.locals');
+    //console.log(res.locals, 'this is general response');
+    //console.log(req.locals, 'this is general request')
+    //console.log(req.body, 'This is from startsession req.locals')
     Session.create({cookieId: res.locals.ssid})
       .then((data) => {
         console.log('successfully created session document');
@@ -20,6 +24,15 @@ sessionController.startSession = (req, res, next) => {
             err: { err: 'Error occurred in creating your session'}
           });
       });
+  }
+
+  sessionController.endSession = (req, res, next) => {
+    //write code here
+    console.log(req.cookies.ssid );
+    res.clearCookie('ssid');
+    res.locals.ssid = ssid;
+     next();
+    //do we need to delete the session from the database too? or just let it expire?
   }
 
 
