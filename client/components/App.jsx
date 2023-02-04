3baa61cf25dd2a3cc5889d6df528d50101057acd
Truman Miller <wesleythemiller@gmail.com>
2023-02-04 15:08:33 -0600

    added bootstrap

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,13 +4,13 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Login from '../containers/LoginPage.jsx';
 // import '../styles.scss';
 import HomeContainer from '../containers/HomePage.jsx';
-import Navbar from './Navbar.jsx';
+import NavbarMain from './NavbarMain.jsx';
 
 const App = () => {
   return (
     <div>
       {/* header to persist through page, can link back to homepage, signout, switchuser */}
-      <Navbar className="navbar" />
+      <NavbarMain className="navbar-main" />
       <h1>WELCOME</h1>
       <Routes>
         {/* Auto load to Login page/> */}
