dc922903331fe0ad3ab9d6c9c0ed7f8ea09c17b8
Mark Krasner <markkrasner@Marks-MacBook-Pro.local>
2023-02-05 16:23:54 -0500

    newest updates for projectControllers

--- server/routes/projectRouter.js
+++ server/routes/projectRouter.js
@@ -3,13 +3,86 @@ const router = express.Router();
 const projectController = require('../controllers/projectController');
 const accountController = require('../controllers/accountController');
 
+//create a project
 router.post('/',
   accountController.checkUser,
   projectController.createProject,
   accountController.addProjectToAccount,
   (req, res) => {
     // console.log(res.locals);
+    return res.status(201).json(res.locals.projects);
+  }
+);
+
+//create a to do list
+router.post('/list',
+  accountController.checkUser,
+  projectController.createToDo,
+  (req, res) => {
+    // console.log(res.locals);
+    return res.status(201).json(res.locals.toDo);
+  }
+);
+
+//create a task
+router.post('/task',
+  accountController.checkUser,
+  accountController.checkUserExists,
+  projectController.createTask,
+  (req, res) => {
+    // console.log(res.locals);
+    return res.status(201).json(res.locals.task);
+  }
+);
+
+//add a user
+router.post('/add',
+  accountController.checkUser,
+  accountController.checkUserExists,
+  projectController.addUserToProject,
+  (req, res) => {
+    // console.log(res.locals);
+    return res.status(201).json(res.locals.newUser);
+  }
+);
+
+//mark task as completed
+router.patch('/complete',
+  accountController.checkUser,
+  projectController.markTaskComplete,
+  (req, res) => {
+    // console.log(res.locals);
+    return res.status(201).json(res.locals.completed);
+  }
+);
+
+//grab all projects corresponding to user
+router.get('/',
+  accountController.checkUser,
+  projectController.getProjects,
+  (req, res) => {
+    // console.log(res.locals);
     return res.status(200).json(res.locals.projects);
+  }
+);
+
+//grab all toDoLists corresponding to project
+router.get('/list',
+  accountController.checkUser,
+  projectController.getToDos,
+  (req, res) => {
+    // console.log(res.locals);
+    return res.status(200).json(res.locals.toDos);
+  }
+);
+
+//get all tasks corresponding to project
+router.get('/task',
+  accountController.checkUser,
+  projectController.getTasks,
+  (req, res) => {
+    // console.log(res.locals);
+    return res.status(200).json(res.locals.tasks);
   }
 );
 
