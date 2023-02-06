42ddf9a50922fc576ad992253abfa53f2df18706
Mark Krasner <markkrasner@Marks-MacBook-Pro.local>
2023-02-06 15:08:33 -0500

    committing for style changes

--- client/containers/MainPage.jsx
+++ client/containers/MainPage.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Img from './removed.png';
+import { useNavigate } from "react-router-dom";
+
 
 const MainPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>
+    <div className="Home-section">
       <h1>ONUS</h1>
-      <p>Beautiful image here</p>
-      <p>Eloquent product blurb here</p>
-      {/* login link should be in the nav bar */}
+  
+      <img src={Img} style={{'width': '30vw', 'margin': '20 auto 100 auto'}}/>
+     
+        
+       
+          <div className = "sign-buttons">
+          <Button variant="primary" size="md" className = "sign-up" onClick={() => navigate("/signup")}>
+          Sign Up
+        </Button>
+        <Button variant="secondary" size="md" className = "log-in" onClick={() => navigate("/login")}>
+          Log In
+        </Button>
+          </div>
+          
+        
+          
+     
+        
+       
     </div>
   );
 };
